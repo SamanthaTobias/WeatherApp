26f554c4e6f109e69779d4722186b2fe6ddd4e20
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-03-19 13:12:28 +0100

    look for API secret in /config/application-secrets/properties

--- src/main/java/com/example/weatherapp/WeatherAppApplication.java
+++ src/main/java/com/example/weatherapp/WeatherAppApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = {AppConfig.class})
 @EnableAspectJAutoProxy
+@PropertySource(value = {"classpath:application.properties", "classpath:application-secrets.properties"})
 public class WeatherAppApplication {
 
 	public static void main(String[] args) {

1320b90c7e9dee46e06ed63d28dab542eab11c32
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-03-21 11:32:23 +0100

    if docker network does not exist, create it

--- build-and-run-weather-app.sh
+++ build-and-run-weather-app.sh
@@ -7,5 +7,10 @@ mvn package
 # Build the Docker image
 docker build -t weather-app .
 
+# Check if my-network exists, create it if it doesn't
+if ! docker network inspect my-network >/dev/null 2>&1; then
+    docker network create my-network
+fi
+
 # Run the Docker container
 docker run --rm --name weather-app --network my-network -p 9020:9020 -e SERVER_PORT=9020 weather-app

191c12243cda3775da0e548c9ae61a7801708817
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-03-21 11:36:53 +0100

    use autowired rather than constructor in controller

--- src/main/java/com/example/weatherapp/controller/WeatherController.java
+++ src/main/java/com/example/weatherapp/controller/WeatherController.java
@@ -1,6 +1,7 @@
 package com.example.weatherapp.controller;
 
 import com.example.weatherapp.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/weather")
 public class WeatherController {
 
-	private final WeatherService weatherService;
-
-	public WeatherController(WeatherService weatherService) {
-		this.weatherService = weatherService;
-	}
+	@Autowired
+	private WeatherService weatherService;
 
 	@GetMapping("/{city}")
 	public ResponseEntity<String> getWeather(@PathVariable("city") String city) {
